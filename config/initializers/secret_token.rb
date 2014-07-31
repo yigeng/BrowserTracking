e66bf82f4d6b8ba4fe53b2a3941530203d4847ee
Yixuan Geng <yixuan@yixuans-mbp.attlocal.net>
2014-07-30 22:30:30 -0700

    created the base app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BrowserFingerprint::Application.config.secret_key_base = '9e60346f1ea60e538495110cf5d5e0834039e012f33f24534e2d65295af890b5811810499b5cbc2f3909385da2ecf0d5ca5c46de52f8e739f781b047bb375b78'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BrowserFingerprint::Application.config.session_store :cookie_store, key: '_browser_fingerprint_session'
